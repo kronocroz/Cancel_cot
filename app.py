74b798993c84eb3d182588a98c90b83c2b6647ba
kronocroz <122318053+kronocroz@users.noreply.github.com>
2025-05-15 16:48:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import sqlite3
+import os 
 
 app = Flask(__name__)
 
@@ -36,6 +37,7 @@ def obtener_cancelaciones():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
+        
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
